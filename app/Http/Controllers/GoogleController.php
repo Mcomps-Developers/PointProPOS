132200f513aaea4cdb505f6e14e04d298a657f68
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-05-16 22:59:07 +0300

    Install Telescope & Php Flasher

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Laravel\Socialite\Facades\Socialite;
 
 class GoogleController extends Controller
@@ -21,25 +22,23 @@ class GoogleController extends Controller
             $user = Socialite::driver('google')->user();
             $finduser = User::where('google_id', $user->id)->first();
 
-            if($finduser){
+            if ($finduser) {
 
                 Auth::login($finduser);
-                return redirect()->intended('home');
-
-            }else{
-                $newUser = User::updateOrCreate(['email' => $user->email],[
-                        'name' => $user->name,
-                        'google_id'=> $user->id,
-                        'password' => encrypt('12345678')
-                    ]);
-
-                Auth::login($newUser);
-
-                return redirect()->intended('home');
+                notyf()
+                    ->position('x', 'right')
+                    ->position('y', 'top')
+                    ->success('Logged in successfully.');
+                return redirect()->intended('dashboard');
+            } else {
+                notyf()
+                    ->position('x', 'right')
+                    ->position('y', 'top')
+                    ->addError('Email not connected to PointPro');
+                return redirect()->intended('login');
             }
-
         } catch (Exception $e) {
-            dd($e->getMessage());
+            Log::error('Error: ',$e->getMessage());
         }
     }
 }
