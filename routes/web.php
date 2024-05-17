707c45b12eb16255d1a69b23cdfea5c5ada5680e
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-05-17 11:07:16 +0300

    Email Verification

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,30 @@
 use App\Http\Controllers\GoogleController;
 use App\Livewire\Admin\AdminDashboard;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 
 
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', AdminDashboard::class)->name('admin.dashboard');
 });
+
+// Email Verification Routes
+    Route::get('/email/verify', function () {
+        return view('auth.verify-email');
+    })->middleware('auth')->name('verification.notice');
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+
+        return redirect('/home');
+    })->middleware(['auth', 'signed'])->name('verification.verify');
+
+    Route::post('/email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
+
+        return back()->with('message', 'Verification link sent!');
+    })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
 
 
 Route::controller(GoogleController::class)->group(function () {
