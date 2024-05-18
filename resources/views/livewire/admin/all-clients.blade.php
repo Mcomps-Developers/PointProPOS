77bdec19768578468e2072996d0696226ea9bc2a
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-05-18 12:38:44 +0300

    Update all-clients.blade.php
    New client modal

--- resources/views/livewire/admin/all-clients.blade.php
+++ resources/views/livewire/admin/all-clients.blade.php
@@ -407,4 +407,113 @@
         </div>
 
     </div>
+    <div class="modal fade" id="add-units">
+        <div class="modal-dialog modal-dialog-centered custom-modal-two">
+            <div class="modal-content">
+                <div class="page-wrapper-new p-0">
+                    <div class="content">
+                        <div class="modal-header border-0 custom-modal-header">
+                            <div class="page-title">
+                                <h4>Add Warehouse</h4>
+                            </div>
+                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body custom-modal-body">
+                            <form action="warehouse.html">
+                                <div class="modal-title-head">
+                                    <h6><span><i data-feather="info" class="feather-edit"></i></span>Warehouse Info</h6>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="mb-3">
+                                            <label class="form-label">Name</label>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="input-blocks">
+                                            <label>Contact Person</label>
+                                            <select class="select">
+                                                <option>Choose</option>
+                                                <option>Steven</option>
+                                                <option>Gravely</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="mb-3 war-add">
+                                            <label class="mb-2">Phone Number</label>
+                                            <input class="form-control" id="phone" name="phone" type="text">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="mb-3">
+                                            <label class="form-label">Work Phone</label>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="mb-3">
+                                            <label class="form-label">Email</label>
+                                            <input type="email" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="modal-title-head">
+                                        <h6><span><i data-feather="map-pin"></i></span>Location</h6>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="mb-3">
+                                            <label class="form-label">Address 1</label>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="mb-3">
+                                            <label class="form-label">Address 2</label>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="input-blocks">
+                                            <label>Country</label>
+                                            <select class="select">
+                                                <option>Choose</option>
+                                                <option>United Kingdom</option>
+                                                <option>United State</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="mb-3">
+                                            <label class="form-label">State</label>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="mb-3 mb-0">
+                                            <label class="form-label">City</label>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="mb-3 mb-0">
+                                            <label class="form-label">Zipcode</label>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-footer-btn">
+                                    <button type="button" class="btn btn-cancel me-2"
+                                        data-bs-dismiss="modal">Cancel</button>
+                                    <button type="submit" class="btn btn-submit">Create Warehouse</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
