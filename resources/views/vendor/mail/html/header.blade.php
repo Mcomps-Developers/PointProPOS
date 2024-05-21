1f710025e8a77abf24ae53119b64c8df67c51970
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-05-21 16:59:41 +0300

    Update header.blade.php
    Adjust logo size in email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,10 +3,11 @@
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
             @if (trim($slot) === 'PointPro')
-            <img src="{{asset('assets/img/logo.png')}}" style="transform: scale(3);" class="logo" alt="PointPro">
+            <img src="{{asset('assets/img/logo.png')}}" style="width: 100%; max-width: 300px;" class="logo" alt="PointPro">
             @else
             {{ $slot }}
             @endif
         </a>
     </td>
 </tr>
+
