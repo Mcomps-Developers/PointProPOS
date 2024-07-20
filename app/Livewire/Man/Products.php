385273c5865e30184d4db1b3647e7ebb1cf3e2ee
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-07-21 00:18:07 +0300

    Products Migration
    Some fields have been left, to be added later

--- app/Livewire/Man/Products.php
+++ app/Livewire/Man/Products.php
@@ -2,12 +2,41 @@
 
 namespace App\Livewire\Man;
 
+use App\Models\Company;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class Products extends Component
 {
+    public function delete($rowID)
+    {
+        try {
+            $product = Product::findOrFail($rowID);
+            $product->delete();
+            notyf()->position('y', 'top')->success('Product deleted successfully');
+            return redirect()->to(request()->header('referer'));
+        } catch (\Illuminate\Database\QueryException $ex) {
+            // Log the database exception
+            Log::error('Database error: ' . $ex->getMessage() . ' in ' . $ex->getFile() . ' on line ' . $ex->getLine());
+
+            // Notify user about the error
+            notyf()->position('y', 'top')->error('Database error occurred. Please try again later.');
+            return redirect()->to(request()->header('referer'));
+        } catch (\Exception $ex) {
+            // Log other exceptions
+            Log::error('Error: ' . $ex->getMessage() . ' in ' . $ex->getFile() . ' on line ' . $ex->getLine());
+
+            // Notify user about the error
+            notyf()->position('y', 'top')->error('An unexpected error occurred. Please try again later.');
+            return redirect()->to(request()->header('referer'));
+        }
+    }
     public function render()
     {
-        return view('livewire.man.products')->layout('layouts.base');
+        $company = Company::where('user_id', Auth::id())->first();
+        $products = Product::orderBy('name')->where('company_id', $company->id)->get();
+        return view('livewire.man.products', ['products' => $products])->layout('layouts.base');
     }
 }
---
+++ database/migrations/2024_07_20_223931_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('company_id');
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->string('sku')->unique();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('image')->nullable();
+            $table->float('price');
+            $table->string('unit');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unique(['company_id', 'name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
