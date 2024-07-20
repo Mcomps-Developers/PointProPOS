f9cfe0a1923c46a136ec2fafa01f19c64047b748
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-07-20 11:44:40 +0300

    Update categories.blade.php

--- resources/views/livewire/man/categories.blade.php
+++ resources/views/livewire/man/categories.blade.php
@@ -167,105 +167,104 @@
             </div>
         </div>
     </div>
-</div>
-
-<div class="modal fade" id="add-category" wire:ignore.self>
-    <div class="modal-dialog modal-dialog-centered custom-modal-two">
-        <div class="modal-content">
-            <div class="page-wrapper-new p-0">
-                <div class="content">
-                    <div class="modal-header border-0 custom-modal-header">
-                        <div class="page-title">
-                            <h4>Create Category</h4>
+    {{-- New Category --}}
+    <div class="modal fade" id="add-category" wire:ignore.self>
+        <div class="modal-dialog modal-dialog-centered custom-modal-two">
+            <div class="modal-content">
+                <div class="page-wrapper-new p-0">
+                    <div class="content">
+                        <div class="modal-header border-0 custom-modal-header">
+                            <div class="page-title">
+                                <h4>Create Category</h4>
+                            </div>
+                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
                         </div>
-                        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body custom-modal-body">
-                        <form wire:submit='create'>
-                            <div class="mb-3">
-                                <label class="form-label">Category</label>
-                                <input type="text" class="form-control" wire:model.live='name' />
-                                @error('name')
-                                <p class="text-danger"> {{$message}}</p>
-                                @enderror
-                            </div>
-                            <div class="mb-3">
-                                <label class="form-label">Category Status</label>
-                                <select name="" class="form-control" wire:model.live='status'>
-                                    <option>--</option>
-                                    <option value="active">Active</option>
-                                    <option value="inactive">Inactive</option>
-                                </select>
-                                @error('status')
-                                <p class="text-danger"> {{$message}}</p>
-                                @enderror
-                            </div>
-                            <div class="modal-footer-btn">
-                                <button type="button" class="btn btn-cancel me-2"
-                                    data-bs-dismiss="modal">Cancel</button>
-                                <button type="submit" class="btn btn-submit" wire:target="create"
-                                    wire:loading.remove>Create Category</button>
-                                <button class="btn btn-warning-light" type="button" disabled wire:target="create"
-                                    wire:loading>
-                                    <span class="spinner-grow spinner-grow-sm align-middle" role="status"
-                                        aria-hidden="true"></span>
-                                    Creating...
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="modal fade" id="edit-category">
-    <div class="modal-dialog modal-dialog-centered custom-modal-two">
-        <div class="modal-content">
-            <div class="page-wrapper-new p-0">
-                <div class="content">
-                    <div class="modal-header border-0 custom-modal-header">
-                        <div class="page-title">
-                            <h4>Edit Category</h4>
-                        </div>
-                        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body custom-modal-body">
-                        <form action="category-list.html">
-                            <div class="mb-3">
-                                <label class="form-label">Category</label>
-                                <input type="text" class="form-control" value="Laptop" />
-                            </div>
-                            <div class="mb-3">
-                                <label class="form-label">Category Slug</label>
-                                <input type="text" class="form-control" value="laptop" />
-                            </div>
-                            <div class="mb-0">
-                                <div
-                                    class="status-toggle modal-status d-flex justify-content-between align-items-center">
-                                    <span class="status-label">Status</span>
-                                    <input type="checkbox" id="user3" class="check" checked />
-                                    <label for="user3" class="checktoggle"></label>
+                        <div class="modal-body custom-modal-body">
+                            <form wire:submit.prevent='create'>
+                                <div class="mb-3">
+                                    <label class="form-label">Category</label>
+                                    <input type="text" class="form-control" wire:model.live='name' />
+                                    @error('name')
+                                    <p class="text-danger"> {{$message}}</p>
+                                    @enderror
                                 </div>
-                            </div>
-                            <div class="modal-footer-btn">
-                                <button type="button" class="btn btn-cancel me-2" data-bs-dismiss="modal">
-                                    Cancel
-                                </button>
-                                <button type="submit" class="btn btn-submit">
-                                    Save Changes
-                                </button>
-                            </div>
-                        </form>
+                                <div class="mb-3">
+                                    <label class="form-label">Category Status</label>
+                                    <select name="" class="form-control" wire:model.live='status'>
+                                        <option>--</option>
+                                        <option value="active">Active</option>
+                                        <option value="inactive">Inactive</option>
+                                    </select>
+                                    @error('status')
+                                    <p class="text-danger"> {{$message}}</p>
+                                    @enderror
+                                </div>
+                                <div class="modal-footer-btn">
+                                    <button type="button" class="btn btn-cancel me-2"
+                                        data-bs-dismiss="modal">Cancel</button>
+                                    <button type="submit" class="btn btn-submit" wire:target="create"
+                                        wire:loading.remove>Create Category</button>
+                                    <button class="btn btn-warning-light" type="button" disabled wire:target="create"
+                                        wire:loading>
+                                        <span class="spinner-grow spinner-grow-sm align-middle" role="status"
+                                            aria-hidden="true"></span>
+                                        Creating...
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    {{-- Edit Category --}}
+    <div class="modal fade" id="edit-category">
+        <div class="modal-dialog modal-dialog-centered custom-modal-two">
+            <div class="modal-content">
+                <div class="page-wrapper-new p-0">
+                    <div class="content">
+                        <div class="modal-header border-0 custom-modal-header">
+                            <div class="page-title">
+                                <h4>Edit Category</h4>
+                            </div>
+                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body custom-modal-body">
+                            <form action="category-list.html">
+                                <div class="mb-3">
+                                    <label class="form-label">Category</label>
+                                    <input type="text" class="form-control" value="Laptop" />
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label">Category Slug</label>
+                                    <input type="text" class="form-control" value="laptop" />
+                                </div>
+                                <div class="mb-0">
+                                    <div
+                                        class="status-toggle modal-status d-flex justify-content-between align-items-center">
+                                        <span class="status-label">Status</span>
+                                        <input type="checkbox" id="user3" class="check" checked />
+                                        <label for="user3" class="checktoggle"></label>
+                                    </div>
+                                </div>
+                                <div class="modal-footer-btn">
+                                    <button type="button" class="btn btn-cancel me-2" data-bs-dismiss="modal">
+                                        Cancel
+                                    </button>
+                                    <button type="submit" class="btn btn-submit">
+                                        Save Changes
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
