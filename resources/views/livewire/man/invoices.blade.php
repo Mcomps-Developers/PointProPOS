ce9b9d8c05b2b5cdeb4c2b8dbf8b1d6abf83e807
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-22 15:00:53 +0300

    Update invoices.blade.php

--- resources/views/livewire/man/invoices.blade.php
+++ resources/views/livewire/man/invoices.blade.php
@@ -162,15 +162,15 @@
                                         <td>{{ $item->customer->name }}</td>
                                         <td style="text-transform: capitalize;">
                                             @if ($item->status === 'progress')
-                                                <span class="text-primary">{{ $item->status }}</span>
+                                                <span class="badge badge-lineprimary">{{ $item->status }}</span>
                                             @elseif ($item->status === 'complete')
-                                                <span class="text-success">{{ $item->status }}</span>
+                                                <span class="badge badge-linesuccess">{{ $item->status }}</span>
                                             @elseif ($item->status === 'cancelled')
-                                                <span class="text-danger">{{ $item->status }}</span>
+                                                <span class="badge badge-linedanger">{{ $item->status }}</span>
                                             @elseif ($item->status === 'defaulted')
-                                                <span class="text-warning">{{ $item->status }}</span>
+                                                <span class="badge badge-linewarning">{{ $item->status }}</span>
                                             @elseif ($item->status === 'pending')
-                                                <span class="text-info">{{ $item->status }}</span>
+                                                <span class="badge badge-lineinfo">{{ $item->status }}</span>
                                             @endif
                                         </td>
                                         <td>KES {{ $item->amount }}</td>
