479c66eb0a1c8439f1e7f5b7ffa0714c7d23625c
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-23 10:22:09 +0300

    Update InvoiceDetails.php

--- app/Livewire/Man/InvoiceDetails.php
+++ app/Livewire/Man/InvoiceDetails.php
@@ -5,9 +5,9 @@ namespace App\Livewire\Man;
 use App\Models\Invoice;
 use App\Models\InvoiceProduct;
 use App\Models\PaymentSchedule;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
-use Carbon\Carbon;
 
 class InvoiceDetails extends Component
 {
@@ -19,10 +19,10 @@ class InvoiceDetails extends Component
     public function render()
     {
         $invoice = Invoice::where('reference', $this->reference)->first();
-
-        $schedules = PaymentSchedule::orderByRaw(DB::raw("STR_TO_DATE(date_due, '%d %b %Y') ASC"))
+        $schedules = PaymentSchedule::orderByRaw(DB::raw("STR_TO_DATE(date_due, '%Y-%m-%d') ASC"))
             ->where('invoice_id', $invoice->id)
             ->get();
+
         $products = InvoiceProduct::where('invoice_id', $invoice->id)->get();
         return view('livewire.man.invoice-details', ['invoice' => $invoice, 'schedules' => $schedules, 'products' => $products])->layout('layouts.base');
     }
