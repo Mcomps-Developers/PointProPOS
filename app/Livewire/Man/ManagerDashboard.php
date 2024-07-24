3f408877d3c62c09d598f430e19266b263129b17
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-24 10:30:42 +0300

    Update ManagerDashboard.php

--- app/Livewire/Man/ManagerDashboard.php
+++ app/Livewire/Man/ManagerDashboard.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire\Man;
 
+use App\Models\Company;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class ManagerDashboard extends Component
 {
     public function render()
     {
-        return view('livewire.man.manager-dashboard')->layout('layouts.base');
+        $company = Company::where('user_id', Auth::id())->first();
+        $products = Product::orderBy('name')->where('company_id', $company->id)->get();
+        return view('livewire.man.manager-dashboard',['products'=>$products])->layout('layouts.base');
     }
 }
