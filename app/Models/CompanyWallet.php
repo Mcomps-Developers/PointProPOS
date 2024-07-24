3037ea045a82c184ffb2163b77af3ff8d461eea7
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-24 12:12:44 +0300

    Create CompanyWallet.php

---
+++ app/Models/CompanyWallet.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CompanyWallet extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class, 'company_id');
+    }
+}
