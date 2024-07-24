5832c848ac31e2bf8f5646ff023f97630a7b4eb6
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-24 12:12:38 +0300

    Update Repayment.php

--- app/Http/Controllers/Repayment.php
+++ app/Http/Controllers/Repayment.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Company;
+use App\Models\CompanyWallet;
 use App\Models\PaymentSchedule;
 use App\Models\Repayment as ModelsRepayment;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class Repayment extends Controller
@@ -20,6 +22,8 @@ class Repayment extends Controller
 
     private function recordTransaction($results)
     {
+        DB::beginTransaction(); // Start a database transaction
+
         try {
             $transaction = new ModelsRepayment();
             $transaction->tracking_id = $results['tracking_id'];
@@ -34,13 +38,17 @@ class Repayment extends Controller
             $transaction->charges = $results['charges'];
             $transaction->failed_code = $results['failed_code'];
             $transaction->failed_reason = $results['failed_reason'];
+
             $repayment = PaymentSchedule::findOrFail($results['api_ref']);
             $transaction->user_id = $repayment->invoice->user_id;
             $transaction->convenience_fee = env('CONVENIENCE_FEE');
             $transaction->company_id = $repayment->invoice->company_id;
+
             $transaction->save();
+
             if ($transaction->state === 'COMPLETE') {
                 $this->createPurchase($transaction);
+                $this->updateCompanyWallet($transaction);
             } else {
                 notyf()
                     ->position('x', 'right')
@@ -48,23 +56,27 @@ class Repayment extends Controller
                     ->addError('Transaction failed.');
                 return redirect(request()->header('Referer'));
             }
+
+            DB::commit(); // Commit the transaction
         } catch (\Exception $e) {
+            DB::rollBack(); // Rollback the transaction if an exception occurs
             notyf()
                 ->position('x', 'right')
                 ->position('y', 'top')
                 ->addError('An error occurred. Please try again later.');
             Log::error('Error saving transaction: ' . $e->getMessage());
         }
+
+        return redirect(request()->header('Referer'));
     }
 
-    // Update Parent Balance
     private function createPurchase($transaction)
     {
-
         try {
             $repayment = PaymentSchedule::findOrFail($transaction->api_ref);
             $repayment->amount_paid += $transaction->value;
             $repayment->save();
+
             if ($repayment->amount - $repayment->amount_paid <= 0) {
                 // Update status to 'paid'
                 $repayment->status = 'paid';
@@ -72,18 +84,18 @@ class Repayment extends Controller
                 $repayment->payment_date = Carbon::now();
                 $repayment->save();
             }
+
             notyf()
                 ->position('x', 'right')
                 ->position('y', 'top')
                 ->success('Transaction successful');
-            return redirect(request()->header('Referer'));
+
         } catch (\Exception $e) {
             Log::error('Unexpected Exception on updating daily parking payment. Details: ' . $e->getMessage());
             notyf()
                 ->position('x', 'right')
                 ->position('y', 'top')
                 ->addError('An error occurred. Please try again later.');
-            return redirect(request()->header('Referer'));
         } catch (\Throwable $th) {
             Log::error('Throwable error on updating daily parking payment. Details: ' . $th->getMessage());
             notyf()
@@ -91,6 +103,31 @@ class Repayment extends Controller
                 ->position('y', 'top')
                 ->addError('An error occurred. Please try again later.');
         }
+    }
 
+    private function updateCompanyWallet($transaction)
+    {
+        try {
+            $wallet = CompanyWallet::findOrFail($transaction->company_id);
+            $wallet->balance += $transaction->value - $transaction->convenience_fee;
+            $wallet->save();
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->success('Company wallet updated.');
+
+        } catch (\Exception $e) {
+            Log::error('Unexpected Exception on updating daily parking payment. Details: ' . $e->getMessage());
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->addError('An error occurred. Please try again later.');
+        } catch (\Throwable $th) {
+            Log::error('Throwable error on updating daily parking payment. Details: ' . $th->getMessage());
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->addError('An error occurred. Please try again later.');
+        }
     }
 }
