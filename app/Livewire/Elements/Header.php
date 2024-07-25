f26d336e830db6e4abba2b9acbce97142f41d348
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-25 17:03:31 +0300

    Update Header.php

--- app/Livewire/Elements/Header.php
+++ app/Livewire/Elements/Header.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire\Elements;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Header extends Component
 {
     public function render()
     {
-        return view('livewire.elements.header');
+        $user = User::findOrFail(Auth::id());
+
+        $unreadNotftbs = $user->unreadNotifications;
+        $allNotfctns = $user->notifications;
+        return view('livewire.elements.header', ['unreadNotftbs' => $unreadNotftbs, 'allNotfctns' => $allNotfctns]);
+    }
+    public function markNotificationAsRead($notificationId)
+    {
+        auth()->user()->notifications()->where('id', $notificationId)->update(['read_at' => now()]);
+    }
+    public function markAllNotificationsAsRead()
+    {
+        auth()->user()->unreadNotifications->markAsRead();
     }
 }
