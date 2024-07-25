a5d34c519da1bb8ea577a47b3a4ac868e174beb7
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-25 19:50:58 +0300

    Update CstDashboard.php

--- app/Livewire/Cst/CstDashboard.php
+++ app/Livewire/Cst/CstDashboard.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Cst;
 
 use App\Models\Invoice;
 use App\Models\PaymentSchedule;
+use App\Models\Repayment;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -13,10 +14,11 @@ class CstDashboard extends Component
     use WithPagination;
     public function render()
     {
-        $invoices = Invoice::where('user_id', Auth::id())->paginate(6);
+        $invoices = Invoice::where('user_id', Auth::id())->limit(6)->get();
         $paidAmount = PaymentSchedule::whereHas('invoice', function ($query) {
             $query->where('user_id', Auth::id());
         })->sum('amount_paid');
-        return view('livewire.cst.cst-dashboard', ['invoices' => $invoices, 'paidAmount' => $paidAmount])->layout('layouts.app');
+        $repayments =Repayment::where('user_id',Auth::id())->limit(4)->get();
+        return view('livewire.cst.cst-dashboard', ['invoices' => $invoices, 'paidAmount' => $paidAmount,'repayments'=>$repayments])->layout('layouts.app');
     }
 }
