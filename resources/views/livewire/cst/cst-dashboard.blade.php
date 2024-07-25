e109c7f0fcb37f9672774db89a2d515feebe619a
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-25 19:40:51 +0300

    Update cst-dashboard.blade.php

--- resources/views/livewire/cst/cst-dashboard.blade.php
+++ resources/views/livewire/cst/cst-dashboard.blade.php
@@ -244,29 +244,29 @@
                 <div class="col-6">
                     <div class="ba-single-history ba-single-history-one"
                         style="background-image: url(cst/img/bg/3.png);">
-                        <h6>Income</h6>
-                        <h5>$58,968.00</h5>
+                        <h6>Save</h6>
+                        <h5>KES {{ $invoices->where('type', 'pay_later')->sum('amount') }}</h5>
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="ba-single-history ba-single-history-two"
                         style="background-image: url(cst/img/bg/3.png);">
-                        <h6>Expenses</h6>
-                        <h5>$50,968.00</h5>
+                        <h6>Credit</h6>
+                        <h5>KES {{ $invoices->where('type', 'collect_later')->sum('amount') }}</h5>
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="ba-single-history ba-single-history-three"
                         style="background-image: url(cst/img/bg/3.png);">
-                        <h6>Total Bills</h6>
-                        <h5>$50,968.00</h5>
+                        <h6>Repayments</h6>
+                        <h5>KES {{ $paidAmount }}</h5>
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="ba-single-history ba-single-history-four"
                         style="background-image: url(cst/img/bg/3.png);">
-                        <h6>Savings</h6>
-                        <h5>$58.00</h5>
+                        <h6>Due</h6>
+                        <h5>KES {{ $invoices->sum('amount') - $paidAmount }}</h5>
                     </div>
                 </div>
             </div>
@@ -274,34 +274,6 @@
     </div>
 </div>
 <!-- history End -->
-
-<!-- cart start -->
-<div class="cart-area pd-top-40">
-    <div class="container">
-        <div class="section-title">
-            <h3 class="title">My Cart</h3>
-            <a href="#">View All</a>
-        </div>
-        <div class="ba-cart-inner" style="background-image: url(cst/img/bg/4.png);">
-            <p>Balance</p>
-            <h4>$56,985.00</h4>
-            <p>Card Number</p>
-            <h5>0000 0000 0000 0909</h5>
-            <div class="row">
-                <div class="col-4">
-                    <p>Expiry</p>
-                    <h5>12/10</h5>
-                </div>
-                <div class="col-8">
-                    <p>CCV</p>
-                    <h5>513</h5>
-                </div>
-            </div>
-            <p>aron smith</p>
-        </div>
-    </div>
-</div>
-<!-- cart End -->
 
 <!-- transaction start -->
 <div class="transaction-area pd-top-40">
