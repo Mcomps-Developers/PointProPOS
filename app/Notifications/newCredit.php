ce8d6fac7f76a51d6a5d3b6c5c9e0ecb351a8d61
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-25 12:51:41 +0300

    Create newCredit.php

---
+++ app/Notifications/newCredit.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class newCredit extends Notification
+{
+    use Queueable;
+    public $user;
+    public $invoice;
+    public $company;
+    public $frequency;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($user, $invoice, $company, $frequency)
+    {
+        $this->user = $user;
+        $this->invoice = $invoice;
+        $this->company = $company;
+        $this->frequency = $frequency;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->success()
+            ->priority(1)
+            ->level('success')
+            ->subject('Congratulations')
+            ->greeting('Congratulations ' . $this->user->name . ',')
+            ->line('Your credit has been processed.')
+            ->action('Dashboard', url('/dashboard'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
