74649bb19b27178ca868c46f0d83f92d9180c23c
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-26 08:08:39 +0300

    Create cst-header.blade.php

---
+++ resources/views/livewire/cst/cst-header.blade.php
@@ -0,0 +1,36 @@
+<div class="header-area" style="background-image: url({{ @asset('cst/img/bg/1.png') }});">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 col-3">
+                <div class="menu-bar">
+                    <i class="fa fa-bars"></i>
+                </div>
+            </div>
+            <div class="text-center col-sm-4 col-4">
+                <a href="{{ route('cst.dashboard') }}" class="logo">
+                    <h3 class="text-white">PointPro</h3>
+                </a>
+            </div>
+            <div class="text-right col-sm-4 col-5">
+                <ul class="header-right">
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-envelope"></i>
+                            <span class="badge">0</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="notification.html">
+                            <i class="fa fa-bell animated infinite swing"></i>
+                            <span class="badge">{{ $unreadNotifications->count() }}</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="header-user" href="javascript:void(0);"><img
+                                src="{{ Auth::user()->profile_photo_url }}"></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
