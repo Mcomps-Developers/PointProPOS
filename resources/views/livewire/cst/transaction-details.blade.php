cabf9a14f4c599b288342a45db18538d486d6ec6
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-26 08:57:24 +0300

    Update transaction-details.blade.php

--- resources/views/livewire/cst/transaction-details.blade.php
+++ resources/views/livewire/cst/transaction-details.blade.php
@@ -11,20 +11,20 @@
     <div class="balance-area pd-top-40">
         <div class="container">
             <div class="section-title">
-                <h3 class="title">Recently Transactios</h3>
+                <h3 class="title">Transaction Details</h3>
                 <a class="sub-delete" href="#"><i class="fa fa-trash"></i></a>
             </div>
             <div class="balance-area-bg bg-transaction-details">
                 <div class="text-center balance-title">
-                    <h6>Here is your transactios <br> details history</h6>
+                    <h6>{{ $transaction->tracking_id }}</h6>
                 </div>
                 <div class="text-center ba-balance-inner"
                     style="background-image: url({{ @asset('cst/img/bg/2.png') }});">
                     <div class="icon">
                         <i class="fa fa-arrow-right"></i>
                     </div>
-                    <h5 class="title">Pyment Sent</h5>
-                    <h5 class="amount">KES {{ number_format($transaction->value) }}</h5>
+                    <h5 class="title">Transaction Amount</h5>
+                    <h5 class="amount">KES {{ number_format($transaction->value, 2) }}</h5>
                 </div>
             </div>
         </div>
@@ -63,8 +63,8 @@
                     <span class="float-right">Repayment</span>
                 </li>
                 <li>
-                    <span class="float-left">Purchase Receipt</span>
-                    <span class="float-right">Yes</span>
+                    <span class="float-left">Return Receipt</span>
+                    <span class="float-right">{{ $transaction->state }}</span>
                 </li>
                 <li>
                     <span class="float-left">Purchase Date</span>
@@ -72,7 +72,7 @@
                 </li>
                 <li>
                     <span class="float-left">Total Amounts</span>
-                    <span class="float-right">$757.00</span>
+                    <span class="float-right">KES {{ number_format($transaction->value, 2) }}</span>
                 </li>
             </ul>
         </div>
@@ -81,7 +81,7 @@
 
     <div class="text-center btn-wrap mg-top-40">
         <div class="container">
-            <p class="btn-content-text">If haveing any transaction issue, Please <a href="javascript:void(0);">contact
+            <p class="btn-content-text">If having any transaction issue, Please <a href="javascript:void(0);">contact
                     us</a>
             </p>
         </div>
