4b48602520fae352aab25a7e35a795019b2ad9c1
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-28 15:27:46 +0300

    Update transactions.blade.php

--- resources/views/livewire/cst/transactions.blade.php
+++ resources/views/livewire/cst/transactions.blade.php
@@ -13,16 +13,32 @@
             </div>
             <ul class="transaction-inner">
                 @foreach ($transactions as $item)
-                @php
-                    $schedule = \App\Models\PaymentSchedule::findOrFail($item->api_ref);
-                @endphp
-                    <li class="ba-single-transaction style-two">
-                        <div class="details">
-                            <h5>INV-{{ $schedule->invoice->reference }}-PP</h5>
-                            <p>{{ date('d M Y', strtotime($schedule->date_due)) }}</p>
-                            <h5 class="amount">KES {{ number_format($item->value, 2) }}</h5>
-                        </div>
-                    </li>
+                    @php
+                        $schedule = \App\Models\PaymentSchedule::findOrFail($item->api_ref);
+                    @endphp
+                    @if ($item->state === 'COMPLETE')
+                        <li class="ba-single-transaction bg-success">
+                            <div class="details">
+                                <h5>INV-{{ $schedule->invoice->reference }}-PP</h5>
+                                <p>Installment: {{ date('d M Y', strtotime($schedule->date_due)) }} |
+                                    @if ($item->state === 'COMPLETE')
+                                        <span class="text-success">Successful</span>
+                                    @else
+                                        <span class="text-danger">Failed</span>
+                                    @endif
+                                </p>
+                                <h5 class="amount">KES {{ number_format($item->value, 2) }}</h5>
+                            </div>
+                        </li>
+                    @else
+                        <li class="ba-single-transaction bg-danger">
+                            <div class="details">
+                                <h5>INV-{{ $schedule->invoice->reference }}-PP</h5>
+                                <p>{{ date('d M Y', strtotime($schedule->date_due)) }}</p>
+                                <h5 class="amount">KES {{ number_format($item->value, 2) }}</h5>
+                            </div>
+                        </li>
+                    @endif
                 @endforeach
 
             </ul>
