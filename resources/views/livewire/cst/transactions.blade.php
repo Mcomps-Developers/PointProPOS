444edf78f2cde493b0be700c3e94798ee73e450c
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-28 15:37:50 +0300

    Update transactions.blade.php

--- resources/views/livewire/cst/transactions.blade.php
+++ resources/views/livewire/cst/transactions.blade.php
@@ -16,33 +16,22 @@
                     @php
                         $schedule = \App\Models\PaymentSchedule::findOrFail($item->api_ref);
                     @endphp
-                    @if ($item->state === 'COMPLETE')
-                        <li class="ba-single-transaction style-two">
-                            <a href="{{ route('transaction.details', ['tracking_id' => $item->tracking_id]) }}">
-                                <div class="details">
-                                    <h5>INV-{{ $schedule->invoice->reference }}-PP</h5>
-                                    <p>Installment: {{ date('d M Y', strtotime($schedule->date_due)) }} |
-                                        @if ($item->state === 'COMPLETE')
-                                            <span class="text-success">Successful</span>
-                                        @else
-                                            <span class="text-danger">Failed</span>
-                                        @endif
-                                    </p>
-                                    <h5 class="amount">KES {{ number_format($item->value, 2) }}</h5>
-                                </div>
-                            </a>
-                        </li>
-                    @else
-                        <li class="ba-single-transaction style-two">
+                    <li class="ba-single-transaction style-two">
+                        <a href="{{ route('transaction.details', ['tracking_id' => $item->tracking_id]) }}">
                             <div class="details">
                                 <h5>INV-{{ $schedule->invoice->reference }}-PP</h5>
-                                <p>{{ date('d M Y', strtotime($schedule->date_due)) }}</p>
-                                <h5 class="amount">KES {{ number_format($item->value, 2) }}</h5>
+                                <p>Installment: {{ date('d M Y', strtotime($schedule->date_due)) }} |
+                                    @if ($item->state === 'COMPLETE')
+                                        <span class="text-success">Successful</span>
+                                    @else
+                                        <span class="text-danger">Failed</span>
+                                    @endif
+                                </p>
+                                <h5 class="amount">KES {{ number_format($item->value) }}</h5>
                             </div>
-                        </li>
-                    @endif
+                        </a>
+                    </li>
                 @endforeach
-
             </ul>
         </div>
     </div>
