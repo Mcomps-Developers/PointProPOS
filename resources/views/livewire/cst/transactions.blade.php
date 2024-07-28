d70e1cc5a47d36dbb18d41e9339079d3ee86e4bd
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-28 15:06:04 +0300

    Update transactions.blade.php

--- resources/views/livewire/cst/transactions.blade.php
+++ resources/views/livewire/cst/transactions.blade.php
@@ -18,4 +18,136 @@
             </div>
         </div>
     </div>
+    <div class="page-title mg-top-50">
+        <div class="container">
+            <a class="float-left" href="{{ route('cst.dashboard') }}">Home</a>
+            <span class="float-right">Transactions</span>
+        </div>
+    </div>
+    <div class="transaction-area pd-top-36">
+        <div class="container">
+            <div class="section-title">
+                <h3 class="title">Recently Transactios</h3>
+                <a href="#"><i class="fa fa-bell"></i></a>
+            </div>
+            <ul class="transaction-inner">
+                <li class="ba-single-transaction style-two">
+                    <div class="details">
+                        <h5>Namecheap Inc.</h5>
+                        <p>Domain Purchase</p>
+                        <h5 class="amount">-$130</h5>
+                    </div>
+                </li>
+                <li class="ba-single-transaction style-two">
+                    <div class="details">
+                        <h5>Namecheap Inc.</h5>
+                        <p>Domain Purchase</p>
+                        <h5 class="amount">-$160</h5>
+                    </div>
+                </li>
+                <li class="ba-single-transaction style-two">
+                    <div class="details">
+                        <h5>Namecheap Inc.</h5>
+                        <p>Domain Purchase</p>
+                        <h5 class="amount">-$890</h5>
+                    </div>
+                </li>
+                <li class="ba-single-transaction style-two">
+                    <div class="details">
+                        <h5>Namecheap Inc.</h5>
+                        <p>Domain Purchase</p>
+                        <h5 class="amount">-$1,000</h5>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- transaction End -->
+
+    <!-- transaction start -->
+    <div class="transaction-area pd-top-36">
+        <div class="container">
+            <div class="section-title">
+                <h3 class="title">Yesterday Transctions</h3>
+            </div>
+            <ul class="transaction-inner">
+                <li class="ba-single-transaction style-two">
+                    <div class="details">
+                        <h5>Namecheap Inc.</h5>
+                        <p>Domain Purchase</p>
+                        <h5 class="amount">-$130</h5>
+                    </div>
+                </li>
+                <li class="ba-single-transaction style-two">
+                    <div class="details">
+                        <h5>Namecheap Inc.</h5>
+                        <p>Domain Purchase</p>
+                        <h5 class="amount">-$160</h5>
+                    </div>
+                </li>
+                <li class="ba-single-transaction style-two">
+                    <div class="details">
+                        <h5>Namecheap Inc.</h5>
+                        <p>Domain Purchase</p>
+                        <h5 class="amount">-$890</h5>
+                    </div>
+                </li>
+                <li class="ba-single-transaction style-two">
+                    <div class="details">
+                        <h5>Namecheap Inc.</h5>
+                        <p>Domain Purchase</p>
+                        <h5 class="amount">-$1,000</h5>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- transaction End -->
+
+    <!-- transaction start -->
+    <div class="transaction-area pd-top-36">
+        <div class="container">
+            <div class="section-title">
+                <h3 class="title">03 Mar 2020</h3>
+            </div>
+            <ul class="transaction-inner">
+                <li class="ba-single-transaction style-two">
+                    <div class="details">
+                        <h5>Namecheap Inc.</h5>
+                        <p>Domain Purchase</p>
+                        <h5 class="amount">-$130</h5>
+                    </div>
+                </li>
+                <li class="ba-single-transaction style-two">
+                    <div class="details">
+                        <h5>Namecheap Inc.</h5>
+                        <p>Domain Purchase</p>
+                        <h5 class="amount">-$160</h5>
+                    </div>
+                </li>
+                <li class="ba-single-transaction style-two">
+                    <div class="details">
+                        <h5>Namecheap Inc.</h5>
+                        <p>Domain Purchase</p>
+                        <h5 class="amount">-$890</h5>
+                    </div>
+                </li>
+                <li class="ba-single-transaction style-two">
+                    <div class="details">
+                        <h5>Namecheap Inc.</h5>
+                        <p>Domain Purchase</p>
+                        <h5 class="amount">-$1,000</h5>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- transaction End -->
+
+    <div class="btn-wrap mg-top-40">
+        <div class="container">
+            <a class="btn-large btn-blue w-100" href="#">More Transctios <i
+                    class="fa fa-angle-double-right"></i></a>
+        </div>
+    </div>
 </div>
