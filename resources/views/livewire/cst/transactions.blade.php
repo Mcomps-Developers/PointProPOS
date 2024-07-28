dbaab93b7d07c4e0a489f7f574141e1c3c582257
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-28 15:03:22 +0300

    Bug Fix

--- resources/views/livewire/cst/transactions.blade.php
+++ resources/views/livewire/cst/transactions.blade.php
@@ -1,3 +1,21 @@
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+    <div class="header-area" style="background-image: url({{ @asset('cst/img/bg/1.png') }});">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 col-3">
+                    <a class="menu-back-page" href="{{ route('cst.dashboard') }}">
+                        <i class="fa fa-angle-left"></i>
+                    </a>
+                </div>
+                <div class="text-center col-sm-4 col-6">
+                    <div class="page-name">Transactions</div>
+                </div>
+                <div class="text-right col-sm-4 col-3">
+                    <div class="search header-search">
+                        <i class="fa fa-search"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
