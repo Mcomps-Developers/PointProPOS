96c3d2318f7617c628d0f7d555f2eb99b3923049
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-28 17:12:10 +0300

    Update cst-dashboard.blade.php

--- resources/views/livewire/cst/cst-dashboard.blade.php
+++ resources/views/livewire/cst/cst-dashboard.blade.php
@@ -95,40 +95,73 @@
             @foreach ($invoices as $item)
                 @if ($item->type === 'pay_later')
                     <div class="single-goal single-goal-three">
-                    @else
-                        <div class="single-goal single-goal-one">
-                @endif
-                <div class="row">
-                    <div class="pr-0 col-7">
-                        <div class="details">
-                            <h6 style="text-transform: uppercase"><a
-                                    href="{{ route('cst.invoice.details', ['reference' => $item->reference]) }}">INV-{{ $item->reference }}-PP</a>
-                            </h6>
-                            <p>
+                        <div class="row">
+                            <div class="pr-0 col-7">
+                                <div class="details">
+                                    <h6 style="text-transform: uppercase"><a
+                                            href="{{ route('cst.invoice.details', ['reference' => $item->reference]) }}">INV-{{ $item->reference }}-PP</a>
+                                    </h6>
+                                    <p>
+                                        @if ($item->type === 'pay_later')
+                                            Credit | {{ $item->company->name }}
+                                        @else
+                                            Saving | {{ $item->company->name }}
+                                        @endif
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="pl-0 col-5">
                                 @if ($item->type === 'pay_later')
-                                    Credit | {{ $item->company->name }}
-                                @else
-                                    Saving | {{ $item->company->name }}
+                                    <div class="circle-inner circle-inner-three">
+                                    @else
+                                        <div class="circle-inner circle-inner-one">
                                 @endif
-                            </p>
-                        </div>
-                    </div>
-                    <div class="pl-0 col-5">
-                        @if ($item->type === 'pay_later')
-                            <div class="circle-inner circle-inner-three">
-                            @else
-                                <div class="circle-inner circle-inner-one">
-                        @endif
-                        <h6 class="goal-amount">KES {{ $item->amount }}</h6>
-                        <div class="chart-circle"
-                            data-value="{{ ($item->repayments->sum('amount_paid') / $item->amount) * 1000 }}">
-                            <canvas width="52" height="52"></canvas>
-                            <div class="text-center chart-circle-value">
-                                {{ number_format(($item->repayments->sum('amount_paid') / $item->amount) * 100) }}%
+                                <h6 class="goal-amount">KES {{ $item->amount }}</h6>
+                                <div class="chart-circle"
+                                    data-value="{{ ($item->repayments->sum('amount_paid') / $item->amount) * 1000 }}">
+                                    <canvas width="52" height="52"></canvas>
+                                    <div class="text-center chart-circle-value">
+                                        {{ number_format(($item->repayments->sum('amount_paid') / $item->amount) * 100) }}%
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                @else
+                    <div class="single-goal single-goal-one">
+                        <div class="row">
+                            <div class="pr-0 col-7">
+                                <div class="details">
+                                    <h6 style="text-transform: uppercase"><a
+                                            href="{{ route('cst.invoice.details', ['reference' => $item->reference]) }}">INV-{{ $item->reference }}-PP</a>
+                                    </h6>
+                                    <p>
+                                        @if ($item->type === 'pay_later')
+                                            Credit | {{ $item->company->name }}
+                                        @else
+                                            Saving | {{ $item->company->name }}
+                                        @endif
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="pl-0 col-5">
+                                @if ($item->type === 'pay_later')
+                                    <div class="circle-inner circle-inner-three">
+                                    @else
+                                        <div class="circle-inner circle-inner-one">
+                                @endif
+                                <h6 class="goal-amount">KES {{ $item->amount }}</h6>
+                                <div class="chart-circle"
+                                    data-value="{{ ($item->repayments->sum('amount_paid') / $item->amount) * 1000 }}">
+                                    <canvas width="52" height="52"></canvas>
+                                    <div class="text-center chart-circle-value">
+                                        {{ number_format(($item->repayments->sum('amount_paid') / $item->amount) * 100) }}%
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
             @endforeach
         </div>
     </div>
