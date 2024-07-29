8d0327807788bc46aee8ab1fe8d5eab08f002eb5
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-29 19:47:13 +0300

    Update contact.blade.php

--- resources/views/livewire/cst/contact.blade.php
+++ resources/views/livewire/cst/contact.blade.php
@@ -13,15 +13,18 @@
     <!-- contact start -->
     <div class="transaction-area pd-top-36">
         <div class="container">
-            <form class="contact-form-wrap">
+            <form wire:submit.prevent='send' class="contact-form-wrap">
                 <p><strong>Note: </strong> This app will pick your bio data to process your request. Communcation routed
                     to email.</p>
                 <ul>
                     <li>
-                        <textarea type="text" rows="20" class="form-control" placeholder="Talk to us...."></textarea>
+                        <textarea type="text" wire:model.live='message' rows="20" class="form-control" placeholder="Talk to us...."></textarea>
                     </li>
                 </ul>
-                <button class="btn-large btn-purple w-100" type="submit">Send Message</button>
+                <button class="btn-large btn-purple w-100" type="submit">
+                    <span wire:loading.remove>Send Message</span>
+                    <span wire:loading>Processing...</span>
+                </button>
             </form>
         </div>
     </div>
