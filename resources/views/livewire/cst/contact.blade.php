4c17b03d8f173b69ef4b30b587e4e35023861482
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-29 19:29:04 +0300

    Update contact.blade.php

--- resources/views/livewire/cst/contact.blade.php
+++ resources/views/livewire/cst/contact.blade.php
@@ -1,3 +1,28 @@
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    @section('title')
+        Contact
+    @endsection
+    <div class="page-title mg-top-50">
+        <div class="container">
+            <a class="float-left" href="{{ route('cst.contact') }}">Home</a>
+            <span class="float-right">Contact</span>
+        </div>
+    </div>
+    <!-- page-title end -->
+
+    <!-- contact start -->
+    <div class="transaction-area pd-top-36">
+        <div class="container">
+            <form class="contact-form-wrap">
+                <p><strong>Note: </strong> This app will pick your bio data to process your request. Communcation routed
+                    to email.</p>
+                <ul>
+                    <li>
+                        <textarea type="text" placeholder="Type Of Message"></textarea>
+                    </li>
+                </ul>
+                <button class="btn-large btn-purple w-100" type="submit">Send Message</button>
+            </form>
+        </div>
+    </div>
 </div>
