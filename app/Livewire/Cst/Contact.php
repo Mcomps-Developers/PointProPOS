c8f1374703bafaa17c83f69a1370664a1d54eed8
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-29 19:47:16 +0300

    Update Contact.php

--- app/Livewire/Cst/Contact.php
+++ app/Livewire/Cst/Contact.php
@@ -6,6 +6,22 @@ use Livewire\Component;
 
 class Contact extends Component
 {
+    public $message;
+
+    protected $rules = [
+        'message' => 'required|string|max:255',
+    ];
+
+    public function updated($fields)
+    {
+        $this->validateOnly($fields);
+    }
+
+    public function send()
+    {
+        $this->validate();
+        
+    }
     public function render()
     {
         return view('livewire.cst.contact')->layout('layouts.app');
