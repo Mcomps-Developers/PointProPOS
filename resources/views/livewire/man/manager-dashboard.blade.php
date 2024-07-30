2fae9d0cc55d93c4be11e7cb4fa7113da6f2ffe4
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-30 04:11:58 +0300

    Update manager-dashboard.blade.php

--- resources/views/livewire/man/manager-dashboard.blade.php
+++ resources/views/livewire/man/manager-dashboard.blade.php
@@ -101,43 +101,6 @@
         </div>
 
         <div class="row">
-            {{-- <div class="col-xl-4 col-sm-12 col-12 d-flex">
-                <div class="card flex-fill">
-                    <div class="card-header d-flex justify-content-between align-items-center">
-                        <h5 class="mb-0 card-title">Purchase & Sales</h5>
-                        <div class="graph-sets">
-                            <ul class="mb-0">
-                                <li>
-                                    <span>Sales</span>
-                                </li>
-                                <li>
-                                    <span>Purchase</span>
-                                </li>
-                            </ul>
-                            <div class="dropdown dropdown-wraper">
-                                <button class="btn btn-light btn-sm dropdown-toggle" type="button"
-                                    id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                                    2023
-                                </button>
-                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <li>
-                                        <a href="javascript:void(0);" class="dropdown-item">2023</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0);" class="dropdown-item">2022</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0);" class="dropdown-item">2021</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div id="sales_charts"></div>
-                    </div>
-                </div>
-            </div> --}}
             <div class="col-xl-12 col-sm-12 col-12 d-flex">
                 <div class="mb-4 card flex-fill default-cover">
                     <div class="card-header d-flex justify-content-between align-items-center">
@@ -159,7 +122,7 @@
                                         <th>Timestamp</th>
                                         <th>Customer</th>
                                         <th>Amount</th>
-                                        <th>Charges</th>
+                                        <th>Charges {{ env('CONVENIENCE_FEE') }}%</th>
                                         <th>Net</th>
                                     </tr>
                                 </thead>
