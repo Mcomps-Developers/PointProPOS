7a6ac1a388283695954d4ea6e0e257a61746d058
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-30 04:06:09 +0300

    Update admin-dashboard.blade.php

--- resources/views/livewire/admin/admin-dashboard.blade.php
+++ resources/views/livewire/admin/admin-dashboard.blade.php
@@ -94,43 +94,6 @@
         </div>
 
         <div class="row">
-            {{-- <div class="col-xl-4 col-sm-12 col-12 d-flex">
-                <div class="card flex-fill">
-                    <div class="card-header d-flex justify-content-between align-items-center">
-                        <h5 class="mb-0 card-title">Purchase & Sales</h5>
-                        <div class="graph-sets">
-                            <ul class="mb-0">
-                                <li>
-                                    <span>Sales</span>
-                                </li>
-                                <li>
-                                    <span>Purchase</span>
-                                </li>
-                            </ul>
-                            <div class="dropdown dropdown-wraper">
-                                <button class="btn btn-light btn-sm dropdown-toggle" type="button"
-                                    id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                                    2023
-                                </button>
-                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <li>
-                                        <a href="javascript:void(0);" class="dropdown-item">2023</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0);" class="dropdown-item">2022</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0);" class="dropdown-item">2021</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div id="sales_charts"></div>
-                    </div>
-                </div>
-            </div> --}}
             <div class="col-xl-12 col-sm-12 col-12 d-flex">
                 <div class="mb-4 card flex-fill default-cover">
                     <div class="card-header d-flex justify-content-between align-items-center">
@@ -176,15 +139,15 @@
                                             </td>
                                             <td>{{ date('d M Y h:iA', strtotime($item->created_at)) }}</td>
                                             <td style="text-transform: capitalize;">{{ $item->customer->name }}</td>
-                                            <td>KES {{ number_format($item->value) }}</td>
+                                            <td>KES {{ number_format($item->value,2) }}</td>
                                             @if ($item->state === 'COMPLETE')
-                                                <td>KES {{ number_format($item->convenience_fee) }}</td>
+                                                <td>KES {{ number_format($item->convenience_fee,2) }}</td>
                                             @else
                                                 <td>KES 0</td>
                                             @endif
 
                                             @if ($item->state === 'COMPLETE')
-                                                <td>KES {{ number_format($item->value - $item->convenience_fee) }}
+                                                <td>KES {{ number_format($item->value - $item->convenience_fee,2) }}
                                                 </td>
                                             @else
                                                 <td>KES 0</td>
